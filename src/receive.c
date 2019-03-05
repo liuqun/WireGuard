dc39c0bd43bb4accf62d608188c5ab2f9073fb34
刘群 <517067180@qq.com>
2019-03-05 11:46:49 +0800

    切换加密解密入口函数
    * chacha20poly1305_encrypt_sg()
    => liuqun_chacha20poly1305_encrypt_sg()
    
    * chacha20poly1305_decrypt_sg()
    => liuqun_chacha20poly1305_decrypt_sg()

--- src/receive.c
+++ src/receive.c
@@ -281,7 +281,7 @@ static bool decrypt_packet(struct sk_buff *skb, struct noise_symmetric_key *key,
 	if (skb_to_sgvec(skb, sg, 0, skb->len) <= 0)
 		return false;
 
-	if (!chacha20poly1305_decrypt_sg(sg, sg, skb->len, NULL, 0,
+	if (!liuqun_chacha20poly1305_decrypt_sg(sg, sg, skb->len, NULL, 0,
 					 PACKET_CB(skb)->nonce, key->key,
 					 simd_context))
 		return false;
